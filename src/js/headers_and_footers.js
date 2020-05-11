a4239507f1d9b1257dfec0f3329d6df283400038
Firkat <dr.firkat@ya.ri>
2020-05-11 23:35:46 +0500

    Header - подгонка под Pefect Pixel

--- src/js/headers_and_footers.js
+++ src/js/headers_and_footers.js
@@ -1,6 +1,8 @@
 import '../sass/header__uikit.sass'
 import '../sass/header.sass'
+import '../sass/footer.sass'
 import '../sass/headers_and_footers.sass'
-import '../sass/colours.sass'
-import '../sass/type.sass'
+import '../css/font-awesome.min.css'
+
+
 console.log('in headers')
